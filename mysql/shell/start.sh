86c30703c9ca2c6a570fd7ecb263697c3b2987fc
Jake Oehler Morrison <jmorrison@psesd.org>
2015-03-24 11:47:15 -0700

    with ip address output

--- mysql/shell/start.sh
+++ mysql/shell/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SERVER_IP_ADDRESS=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+echo "Server IP Address: $SERVER_IP_ADDRESS\n\n"
 
 if [ ! -f /mysql-configured ]; then
   touch /mysql-configured
--- redis/shell/start.sh
+++ redis/shell/start.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+SERVER_IP_ADDRESS=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+echo "Server IP Address: $SERVER_IP_ADDRESS\n\n"
