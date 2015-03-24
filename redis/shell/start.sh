082d68a4fe7d47b9ed6da19520fdbae128894738
Jake Oehler Morrison <jmorrison@psesd.org>
2015-03-24 11:49:57 -0700

    minor fix

--- redis/shell/start.sh
+++ redis/shell/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SERVER_IP_ADDRESS=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-echo "Server IP Address: $SERVER_IP_ADDRESS\n\n"
+echo "Redis Server IP Address: $SERVER_IP_ADDRESS\n\n"
